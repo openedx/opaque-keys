f9a74f0f4f32064587f8142408a48be2efd55317
Calen Pennington <cale@edx.org>
2014-08-01 11:25:55 -0400

    Always call super(..).__init__(..) from DefinitionLocator.__init__

--- opaque_keys/edx/locator.py
+++ opaque_keys/edx/locator.py
@@ -863,16 +863,13 @@ class DefinitionLocator(Locator, DefinitionKey):
     definition_id = None
 
     def __init__(self, block_type, definition_id):
-        if isinstance(definition_id, LocalId):
-            super(DefinitionLocator, self).__init__(definition_id=definition_id, block_type=block_type)
-        elif isinstance(definition_id, basestring):
+        if isinstance(definition_id, basestring):
             try:
                 definition_id = self.as_object_id(definition_id)
             except ValueError:
                 raise InvalidKeyError(self, definition_id)
-            super(DefinitionLocator, self).__init__(definition_id=definition_id, block_type=block_type)
-        elif isinstance(definition_id, ObjectId):
-            super(DefinitionLocator, self).__init__(definition_id=definition_id, block_type=block_type)
+
+        super(DefinitionLocator, self).__init__(definition_id=definition_id, block_type=block_type)
 
     def _to_string(self):
         '''
