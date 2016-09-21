d727901a2bb4843d1237b932546911e9344b8391
Calen Pennington <cale@edx.org>
2016-09-21 11:59:24 -0400

    Don't allow deprecated BlockUsageLocators to use anything but an i4x prefix

--- opaque_keys/edx/tests/test_properties.py
+++ opaque_keys/edx/tests/test_properties.py
@@ -167,6 +167,7 @@ def perturbed_strings(string_strategy):
     perturbed='aside-usage-v1:lib-block-v1:$:$:--$:+-$:$:$:+branch@-$:-'
               '+version@000000000000000000000000+type@$:+block@$:::0',
 )
+@example(key_type=UsageKey, serialized='i4x://-/-/0/-', perturbed='0i4x://-/-/0/-')
 def test_perturbed_serializations(key_type, serialized, perturbed):
     assume(serialized != perturbed)
 
