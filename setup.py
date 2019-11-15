5aff0a3d768acd0275c23d87d534bb9a5b873e1f
aarif <mraarif@outlook.com>
2019-11-15 11:42:04 +0500

    bumped up the package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='edx-opaque-keys',
-    version='2.0.0',
+    version='2.0.1',
     author='edX',
     url='https://github.com/edx/opaque-keys',
     classifiers=[
