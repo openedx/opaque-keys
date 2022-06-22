f53ac43c6068a045fb03a7ec0e4eaccbcd684bb2
rayzhou-bit <rzhou@2u.com>
2022-06-22 19:40:26 -0400

    chore: pylint syntax

--- opaque_keys/tests/test_opaque_keys.py
+++ opaque_keys/tests/test_opaque_keys.py
@@ -99,7 +99,7 @@ class DictKey(DummyKey):
             raise InvalidKeyError(cls, serialized) from error
 
     def __hash__(self):
-        # pylint: disable-next: consider-using-generator
+        # pylint: disable-next=consider-using-generator
         return hash(type(self)) + sum([hash(elt) for elt in self.value.keys()])  # pylint: disable=no-member
 # pylint: enable=abstract-method
 
